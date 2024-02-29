42b2557c073a100d7721171aec4d8a4e47797bbc
Thanh Duy <phamnguyenthanhduylxag@gmail.com>
2024-02-29 14:52:12 +0700

    update
    up

---
+++ BTL_LTWeb/Services/BaseService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BTL_LTWeb.Models;
+
+
+namespace BTL_LTWeb.Services
+{
+    public class BaseService
+    {
+        public QL_QUANAOEntities2 db = new QL_QUANAOEntities2();
+    }
+}
